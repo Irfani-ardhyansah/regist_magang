b806bad829fb7feec8c471d098fd1a5f76fbd39d
Irfani-ardhyansah <mohamadfani99@gmail.com>
2020-09-02 14:21:02 +0700

    commit

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -141,7 +141,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal Edit</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Modal Tambah Anggota</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -245,7 +245,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal Edit</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Modal Edit Anggota</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -346,7 +346,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal Edit</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Modal Edit Kelompok</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
